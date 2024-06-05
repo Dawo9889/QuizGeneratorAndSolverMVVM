9f230e18a5cd031eff16407ac59c542e1717e723
Dawo9889 <dawid.spolecznosci@gmail.com>
2024-06-05 20:50:01 +0200

    EditViewModel

--- PierwszePodejscieDoQuizu/View/EditWindow.xaml.cs
+++ PierwszePodejscieDoQuizu/View/EditWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PierwszePodejscieDoQuizu.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace PierwszePodejscieDoQuizu.View
         public EditWindow()
         {
             InitializeComponent();
+            DataContext = new EditWindowViewModel();
         }
     }
 }
---
+++ PierwszePodejscieDoQuizu/ViewModel/EditWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierwszePodejscieDoQuizu.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class EditWindowViewModel : Base.BaseViewModel
+    {
+        public EditWindowViewModel()
+        {
+            
+        }
+    }
+}
